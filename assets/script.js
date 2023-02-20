b3e10b0123899b820f2c711e6bbdcdc77d0ccd61
James Martin <jamesmartin@Jamess-MBP.lan>
2023-02-19 23:47:22 -0500

    coding quiz updates

---
+++ assets/script.js
@@ -0,0 +1,79 @@
+// create a start button for the quiz
+// create a timer for the quiz
+// create series of questions with timer subtracting after incorrect answers 
+// when all questions are answered timer reaches 0 and quiz is over
+// when game is over I can save my initials and score
+
+const quizData = [ 
+{
+    question: "What does CSS stand for?",
+    a: "Car Styling Straps",
+    b: "Cascading Single Sheets",
+    c: "Cascading Style Sheets",
+    d: "Cats Standing Still",
+    correct: "c",
+},
+
+{
+    question: "What does HTML stand for?",
+    a: "Humans Talk Much Longer",
+    b: "Hypertext Markup language",
+    c: "How To Make Lasagna",
+    d: "Hypertext Mixup Language",
+    correct: "b",
+
+},
+
+{
+    question: "True of False? Java and Javascript mean the same thing?",
+    a: "True",
+    b: "False",  
+    correct: "b",
+},
+
+{
+    question: "What are values that evaluate to true or False?",
+    a: "String",
+    b: "Null",
+    c: "Undefined",
+    d: "Boolean",
+    correct: "d",
+
+}
+];
+let currentQuiz = 0;
+let score = 0;
+
+let maxTime = 40;
+function startTimer(){
+    document.getElementById('timer').innerHTML = 'timer: ' + maxTime;
+    
+    if 
+}
+
+
+
+
+
+ 
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
