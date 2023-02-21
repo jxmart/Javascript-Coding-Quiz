323559075707c43d2fb4ee1557ad3aa3f327a979
James Martin <jamesmartin@Jamess-MBP.lan>
2023-02-21 16:42:53 -0500

    updated all files for coding quiz

--- assets/script.js
+++ assets/script.js
@@ -3,44 +3,82 @@
 // create series of questions with timer subtracting after incorrect answers 
 // when all questions are answered timer reaches 0 and quiz is over
 // when game is over I can save my initials and score
+let highScores = document.querySelector("#highscores")
+let timer = document.querySelector("#timer")
+let intro = document.querySelector('#intro')
+let start = document.querySelector('#start')
+let question = document.querySelector('#question')
+let choices = document.querySelector('#choices')
+let answer = document.querySelector('#answer')
+let score = document.querySelector('#score')
+let initials = document.querySelector('#initials')
+let end = document.querySelector('#end')
+console.log(choices)
 
+
+let secondsLeft = 60
+let qindex = 0
 const quizData = [ 
 {
     question: "What does CSS stand for?",
     answers: ['Collecting Silly Stamps', 'Cascading Style Sheets', 'Cascading Single Sheet', 'Cats Standing Still'],
-    correctAnswer: 'Cascading Style Sheets',
-   
- 
+    correctAnswer: '1',
 },
-
 {
     question: "What does HTML stand for?",
     answers: ['Humans Take Much Longer', 'Hypertext Mixup Language', 'How to Make Lasagna', 'Hypertext Markup Language'],
-    correctAnswer: 'Hypertext Markup Language',
-
+    correctAnswer: '3',
 },
-
 {
     question: "True of False? Java and Javascript mean the same thing?",
     answers: ['True', 'False'],  
-    correctAnswer: "False",
+    correctAnswer: "1",
 },
-
 {
     question: "What is the value that evaluates to true or False?",
     answers: ['String', 'Null', 'Undefined', 'Boolean'],
-    correctAnswer: "Boolean",
-
+    correctAnswer: "3",
 },
-];
-let currentQuiz = 0;
-let score = 0;
+]; 
+start.addEventListener('click', function(){
+    startTimer()
+    startGame()
+}) 
 
-let maxTime = 40;
+
+
 function startTimer(){
-    document.getElementById('timer').innerHTML = 'timer: ' + maxTime;
-    
+    let timerInterval = setInterval(function() {
+        secondsLeft--
+        timer.textContent = 'Time Left: ' + secondsLeft 
+
+        if (secondsLeft < 1) {
+            clearInterval(timerInterval)
+            alert ("Stop")
+        }
+    }, 1000)
 }
+
+function startGame(){
+    document.querySelector('#intro').style.display = "none"
+    document.querySelector('#begin').style.display = "block"
+
+    getQuestion()
+}
+
+function getQuestion (){
+    question.textContent = quizData[qindex].question
+
+    for (let i = 0; i < quizData[qindex].answers.length; i++) {
+        const button = document.createElement('button')
+        button.textContent = quizData[qindex].answers[i]
+        button.id = (i + 1).toString()
+        question.append(button)
+        // button.addEventListener('click', )
+    }
+}
+    
+
 
 
 
